21e619ad710df46287800fcad431eb4c95340d48
aminstu <amin.ben-errabeh@stu.ocsb.ca>
2022-11-24 12:46:47 -0500

    FINISHED

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,46 +2,59 @@ package shapes;
 
 public class Circle {
 
-	private double radius;
-	private double circumference;
-	private double area;
-	
-	//Methods
-	public Circle() {
-		
-	}
-	
-	public Circle(double r) {
-		
-	}
-	
-	private double findCircumference() {
-		return Math.PI*;
-	}
-	
-	private double findArea() {
-		return ;
-	}
-	
-	public void printStats() {
-		
-	}
+private double radius;
+private double circumference;
+private double area;
 
-	public double getRadius() {
-		return radius;
-	}
+//Methods
+public Circle() {
+radius = 1;
+circumference=findCircumference();
+area=findArea();
+}
 
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
+public Circle(double r) {
+	this.radius =r;
+	circumference=findCircumference();
+	area=findArea();  
 
-	public double getCircumference() {
-		return circumference;
-	}
 
-	public double getArea() {
-		return area;
-	}
-	
-	
+
+}
+
+private double findCircumference() {
+return Math.PI * radius * 2;
+}
+
+private double findArea() {
+area= Math.PI* Math.pow(radius, 2);
+return area;
+}
+
+public void printStats() {
+System.out.println("Circle");
+System.out.println("Radius ="+this.radius);
+System.out.println("Circumference ="+this.circumference);
+System.out.println("Area ="+this.area);
+}
+
+public double getRadius() {
+
+return radius;
+}
+
+
+public void setRadius(double radius) {
+this.radius = radius;
+}
+
+public double getCircumference() {
+return circumference;
+}
+
+public double getArea() {
+return area;
+}
+
+
 }
--- src/shapes/Main.java
+++ src/shapes/Main.java
@@ -7,20 +7,52 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
 		int userChoice;
+		Triangle Tria;
+		Circle Circ;
+		Rectangle Rect;
+		Scanner in = new Scanner(System.in);
+
 		do {
 			System.out.println("Do you want to:");
-			System.out.println("1. Create a shape");
-			System.out.println("2. View a shape");
-			System.out.println("3. Change a shape");
+			System.out.println("1. Create a Triangle");
+			System.out.println("2. Create a Circle");
+			System.out.println("3. Create a Rectangle");
 			System.out.println("4. Quit");
 			try {
 				userChoice = in.nextInt();
 			} catch (InputMismatchException e) {
 				userChoice = 4;
 				in.nextLine();
+
 			}
+		
+				
+
+			if (userChoice == 1) {
+				System.out.println("CREATING TRIANGLE");
+				System.out.println("Enter values for sides A, B and C correspondingly");
+				Tria = new Triangle(in.nextDouble(), in.nextDouble(), in.nextDouble());
+				Tria.printStats();
+			} else if (userChoice == 2) {
+				System.out.println("You selected CIRCLE");
+				System.out.println("Enter a value for the radius");
+				Circ = new Circle(in.nextDouble());
+				Circ.printStats();
+			} else if (userChoice == 3) {
+				System.out.println("CREATING RECTANGLE");
+				System.out.println("Enter values for the length and width correspondingly");
+				Rect = new Rectangle(in.nextDouble(), in.nextDouble());
+				Rect.printStats();
+
+			} else if (userChoice == 4) {
+				System.out.println("Program Exited, Rerun To Start Over");
+				System.exit(0);
+
+			}
+
 		} while (userChoice != 4);
+		System.out.println("You have exited the program");
+
 	}
 }
